eea62278c00e0a8a39d17491b7939709715e8f90
Linell Bonnette <tlbonnette@gmail.com>
2020-06-19 16:29:45 -0500

    feat: basic torch admin integration

--- lib/kudzu_web/endpoint.ex
+++ lib/kudzu_web/endpoint.ex
@@ -24,6 +24,13 @@ defmodule KudzuWeb.Endpoint do
     gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
+  # Torch, a la Active Admin
+  plug Plug.Static,
+    at:                      "/torch",
+    from:                    { :torch, "priv/static" },
+    gzip:                    true,
+    cache_control_for_etags: "public, max-age=86400"
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
